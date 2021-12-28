09b54f37319c4707229e360da55479cc2e277dd7
gfouzie <gfouzie@uwaterloo.ca>
2021-12-27 19:59:35 -0500

    working version | white background

--- src/public/chart.js
+++ src/public/chart.js
@@ -10,7 +10,16 @@ let RSA = options["reassigning_sexual_attitudes"];
 let ALL = options["all"];
 
 window.onload = function () {
-  // Bar chart
+  //show and hide graphs when the window loads and reset the button
+  var doughnut = document.getElementById("doughnutDiv");
+  var bar = document.getElementById("barDiv");
+  var btn = document.getElementById("swapGraphBtn");
+  btn.innerHTML = "View Bar Graph";
+  doughnut.style.display = "block";
+  bar.style.display = "none";
+  barShowing = true;
+
+  // Doughnut chart
   new Chart(document.getElementById("doughnut-chart"), {
     type: "doughnut",
     data: {
@@ -22,18 +31,92 @@ window.onload = function () {
       ],
       datasets: [
         {
-          label: "Population (millions)",
+          backgroundColor: ["#B67CDF", "#FBE26F", "#7BDFB6", "#94DE7D"],
+          data: [MH, SS, RSA, ALL],
+        },
+      ],
+    },
+    options: {
+      legend: {
+        display: true,
+        labels: {
+          fontSize: 20,
+        },
+      },
+      title: {
+        display: true,
+        text: "What Taught You the Most?",
+        fontSize: 50,
+        padding: 25,
+      },
+    },
+  });
+
+  // Bar chart
+  new Chart(document.getElementById("bar-chart"), {
+    type: "bar",
+    data: {
+      labels: [
+        "Menstrual Health",
+        "Safe Sex",
+        "Reassigning Sexual Attitudes",
+        " All",
+      ],
+      datasets: [
+        {
           backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9"],
           data: [MH, SS, RSA, ALL],
         },
       ],
     },
     options: {
-      legend: { display: true },
+      legend: { display: false },
       title: {
         display: true,
-        text: "tf is the question",
+        text: "What Taught You the Most?",
+        fontSize: 50,
+        padding: 25,
+      },
+      scales: {
+        scaleLabel: { fontSize: 50 },
+        xAxes: [
+          {
+            ticks: {
+              fontSize: 20,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              min: 0,
+              stepSize: 1,
+            },
+          },
+        ],
       },
     },
   });
 };
+
+var barShowing = false;
+
+function swapGraph() {
+  var doughnut = document.getElementById("doughnutDiv");
+  var bar = document.getElementById("barDiv");
+  var btn = document.getElementById("swapGraphBtn");
+  console.log("in function");
+  if (barShowing == true) {
+    btn.innerHTML = "View Pie Chart";
+    doughnut.style.display = "none";
+    bar.style.display = "block";
+    barShowing = false;
+    console.log("bar showing true");
+  } else {
+    btn.innerHTML = "View Bar Graph";
+    doughnut.style.display = "block";
+    bar.style.display = "none";
+    barShowing = true;
+    console.log("bar showing false");
+  }
+}
